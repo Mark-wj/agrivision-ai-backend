da7fe6c9984663308aaedd34814e3da1070e2c11
Mark Njenga <marknjenga25@gmail.com>
2025-11-16 21:44:17 +0300

    Add deployed frontend link

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000", 
         "http://localhost:3001",
-        "https://agrivision-ai-frontendd.vercel.app/"
+        "https://agrivision-ai-frontendd.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
